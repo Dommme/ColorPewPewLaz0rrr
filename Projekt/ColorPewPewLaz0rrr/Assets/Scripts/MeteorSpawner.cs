c5298e1eff19db66b215c48680a70e1d76aa0ec7
nyccc <40388055+nyccc@users.noreply.github.com>
2018-08-11 14:51:56 +0200

    Meteor Prefabs + MeteorSpawner
    Rudimentäres Meteor Prefab erstellt; MeteorController Script (bisher Forwärtsbewegung, Drehung und Zerstörung der vorbeigeflogenen Objekte)
    MeteorSpawner GameObject mit MeteorSpawner Script (Zufällige Instanziierung der Meteore in bestimmten Spawnbereich in regelmäßigen Abständen)
    Ordnerstruktur überarbeitet

---
+++ Projekt/ColorPewPewLaz0rrr/Assets/Scripts/MeteorSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorSpawner : MonoBehaviour
+{
+    public GameObject meteor;
+    public Vector3 spawnValues;
+    public float spawnDelay;
+    public float startDelay;
+
+    void Start()
+    {
+        StartCoroutine(SpawnMeteors());
+    }
+
+    IEnumerator SpawnMeteors()
+    {
+        //Countdown bis Beginn der Spawnphase
+        yield return new WaitForSeconds(startDelay);
+
+        //Spawnphase
+        // TODO: Loop condition hinzufügen!
+        while (true) {
+            //Spawnbereich
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), Random.Range(-spawnValues.y, spawnValues.y), spawnValues.z);
+            Quaternion spawnRotation = Quaternion.identity;
+            Instantiate(meteor, spawnPosition, spawnRotation);
+            //Wartezeit zwischen Spawns
+            yield return new WaitForSeconds(spawnDelay);
+        }
+    }
+}
