cac596e5e8f61437c28fb8bfc7ae0ce18a0f6f6f
nyccc <40388055+nyccc@users.noreply.github.com>
2018-08-29 21:31:59 +0200

    Untergrund reworked
    Untergrund-Grid komplett überarbeitet; statt Instanzierung mehrerer Planes hintereinander und Bewegen per Rigidbody.velocity nun eine große Plane als geloopte Animation (keine ungenauen Trigger Events mehr + kein Abprallen der einzelnen Planes untereinander --> keine Lücken im Untergrund mehr; flexibles Anpassen der Geschwindigkeit zur Laufzeit per neuem gridSpeedController-Skript möglich)

---
+++ Projekt/ColorPewPewLaz0rrr/Assets/Scripts/gridSpeedController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gridSpeedController : MonoBehaviour {
+
+    public float gridSpeed;
+    Animator gridAnimator;
+
+    void Start()
+    {
+        gridAnimator = this.GetComponent<Animator>();
+        gridAnimator.speed = gridSpeed;
+    }
+}
