73ed7c964980e4c891c198830ac2a631aba5f644
Florian Arpe <florian-ar@gmx.de>
2018-09-18 19:28:11 +0200

    Logo anpassung

--- Projekt/ColorPewPewLaz0rrr/Assets/Scripts/UIscript.cs
+++ Projekt/ColorPewPewLaz0rrr/Assets/Scripts/UIscript.cs
@@ -16,63 +16,70 @@ public RawImage[] UILifes;
     public TMP_Text ScoreProText;
 
  
-    private int countScore = 0;
-    public int countLife = 3;
+    private int countScore = 0;         //Anfangsscore bei Beginn des Spiels
+    public int countLife = 3;           //Anzahl an Leben. Hier an GUI angepasst
 	void Start () {
         countScore=0;
         
-        UILifes[0].enabled=false; //0Life
-        UILifes[1].enabled=false; //1Life
-        UILifes[2].enabled=false; //2Life 
-        UILifes[3].enabled=true;  //3Life 
-        
-        UIColors[0].enabled=true;  //Non
-        UIColors[1].enabled=false; //Blue
-        UIColors[2].enabled=false; //Yellow
-        UIColors[3].enabled=false; //Red
+            //Anfangszustand GUI
+        UILifes[0].enabled=false;       //GUI bei GameOver
+        UILifes[1].enabled=false;       //GUI bei einem verbleibendem Leben
+        UILifes[2].enabled=false;       //GUI bei zwei verbleibenden Leben
+        UILifes[3].enabled=true;        //GUI bei drei verbleibenden Leben, also bei Beginn des Spiels   
+        UIColors[0].enabled=true;       //Keine Farbe des Lasers ausgewählt, Normalzustand bei Beginn des Spiels
+        UIColors[1].enabled=false;      //Blauer Laser ausgewählt
+        UIColors[2].enabled=false;      //Gelber Laser ausgewählt
+        UIColors[3].enabled=false;      //Roter Laser ausgewählt
         
         
 	}
-	
+	   //GUI Änderung bei Auswahl des roten Lasers
    public void SelectRed(){
         UIColors[0].enabled=false;
         UIColors[1].enabled=false; 
         UIColors[2].enabled=false;
         UIColors[3].enabled=true;
        }
+        //GUI Änderung bei Auswahl des blauen Lasers
     public void SelectBlue(){
         UIColors[0].enabled=false;
         UIColors[1].enabled=true; 
         UIColors[2].enabled=false;
         UIColors[3].enabled=false;
     }
+        //GUI Änderung bei Auswahl des gelben Lasers
      public void SelectYellow(){
         UIColors[0].enabled=false;
         UIColors[1].enabled=false; 
         UIColors[2].enabled=true;
         UIColors[3].enabled=false;
     }
+        //GUI Änderung bei Auswahl keines Lasers
     public void SelectNone(){
         UIColors[0].enabled=true;
         UIColors[1].enabled=false; 
         UIColors[2].enabled=false;
         UIColors[3].enabled=false;
        }
+        //GUI Änderung bei verlieren von Leben
 	 public void Life(){
-        countLife--;
+        countLife--;                    //Leben wird runtergezählt.
         
+        //GUI Änderung bei verbleibenden 2 Leben
         if(countLife==2){
             UILifes[0].enabled=false;
             UILifes[1].enabled=false;
             UILifes[2].enabled=true;
             UILifes[3].enabled=false;    
         }
+         //GUI Änderung bei verbleibendem Leben
         if(countLife==1){
             UILifes[0].enabled=false;
             UILifes[1].enabled=true;
             UILifes[2].enabled=false;
             UILifes[3].enabled=false;    
         }
+         //GUI Änderung bei keinem Leben (Game Over)
          if(countLife==0){
             UILifes[0].enabled=true;
             UILifes[1].enabled=false;
@@ -83,14 +90,15 @@ public RawImage[] UILifes;
     
         
     }
+        //Getter für countLife Variable
     public int getLife(){
         return countLife;
         
     }
-    // Update is called once per frame
+        // Update is called once per frame
 	void Update () {
 	}
-
+        //Score Erhöhung wird pro frame ausgeführt und in GameState Handler überführt
     public void Score(int value){
         countScore += value;
         ScoreProText.text= countScore.ToString();
